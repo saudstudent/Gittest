be3df7698c0d1677af5dd0ba8cf5157b5c66f69a
Saud Baig <165429@s020rmc148-13.local>
2018-11-12 13:55:04 -0600

    Initial Commit

---
+++ Gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gittest
+//
+//  Created by Baig, Saud S on 11/12/18.
+//  Copyright © 2018 Baig, Saud S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ GittestTests/GittestTests.swift
@@ -0,0 +1,34 @@
+//
+//  GittestTests.swift
+//  GittestTests
+//
+//  Created by Baig, Saud S on 11/12/18.
+//  Copyright © 2018 Baig, Saud S. All rights reserved.
+//
+
+import XCTest
+@testable import Gittest
+
+class GittestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
